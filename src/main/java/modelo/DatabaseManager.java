830fde37e71ce7314c15b59b1bddecc627fc9732
KeyKoder <victor.fernandez.montano@gmail.com>
2025-03-03 23:17:37 +0100

    Crear DatabaseManager

---
+++ src/main/java/modelo/DatabaseManager.java
@@ -0,0 +1,8 @@
+package modelo;
+
+/**
+ * Esta clase se encarga de mantener la conexión con la base de datos y de manejar las transacciones con dicha base de
+ * datos.
+ */
+public class DatabaseManager {
+}
