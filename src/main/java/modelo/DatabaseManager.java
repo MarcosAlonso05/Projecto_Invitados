969fd0c7a90f86e473499ad2587b60ff97c5c350
KeyKoder <victor.fernandez.montano@gmail.com>
2025-03-03 23:20:51 +0100

    Añadir Hibernate al pom.xml

--- src/main/java/modelo/DatabaseManager.java
+++ src/main/java/modelo/DatabaseManager.java
@@ -1,8 +1,13 @@
 package modelo;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 /**
  * Esta clase se encarga de mantener la conexión con la base de datos y de manejar las transacciones con dicha base de
  * datos.
  */
 public class DatabaseManager {
+	private SessionFactory sessionFactory;
+	private Session session;
 }
